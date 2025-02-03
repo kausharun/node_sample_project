9240dfae93bc449a66726adbf7d757729c3e8463
kausharun <kaush.arunkumar@gmail.com>
2025-02-02 23:05:47 -0800

    use a callback for nonblocking implementation

--- read.js
+++ read.js
@@ -1,5 +1,7 @@
 const { readFile, readFileSync } = require('fs');
 
-const txt = readFileSync('./hello.txt', 'utf8');
+readFile('./hello.txt', 'utf8', (err, txt) => {
+    console.log(txt)
+})
 
-console.log(txt)
+console.log("This is urgent")
