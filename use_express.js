b65335ab1909ad5e06a9b384670e398b386e2d89
kausharun <kaush.arunkumar@gmail.com>
2025-02-02 23:48:29 -0800

    implement promises

--- use_express.js
+++ use_express.js
@@ -1,16 +1,11 @@
-const { readFile, readFileSync } = require('fs');
+const { readFile, readFileSync } = require('fs').promises;
 
 const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    readFile('./page.html', 'utf8', (err, html) => {
-        if (err) {
-            response.status(500).send('Out of order')
-        }
-        response.send(html);
-    })
+app.get('/', async (request, response) => {
+    response.send( await readFile('./page.html') );
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`Page available at http://localhost:3000`));
